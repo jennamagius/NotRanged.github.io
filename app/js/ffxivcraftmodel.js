c21b70b05c68e3e7eb4f2cb81e1ef3659286d825
jennamagius <7319967+jennamagius@users.noreply.github.com>
2021-11-14 21:25:31 -0700

    Update ffxivcraftmodel.js

--- app/js/ffxivcraftmodel.js
+++ app/js/ffxivcraftmodel.js
@@ -1349,6 +1349,16 @@ function evalSeq(individual, mySynth, penaltyWeight) {
 
     // Check for feasibility violations
     var chk = result.checkViolations();
+    try {	
+    if (result.state.qualityState < result.synth.recipe.maxQuality) {
+	    fitness -= 50000;
+    } else if (chk.progressOK && chk.durabilityOK) {
+	    var number_of_steps_weve_saved = 50 - result.step;
+	    fitness += 1000 * number_of_steps_weve_saved;
+    }
+    } catch(e) {
+	    console.log("Failure", e);
+    }
 
     if (!chk.durabilityOk) {
        penalties += Math.abs(result.durabilityState);
