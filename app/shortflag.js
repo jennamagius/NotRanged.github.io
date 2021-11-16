57747957056e4ff8927dc8d37a4067bdd7fec095
jennamagius <7319967+jennamagius@users.noreply.github.com>
2021-11-15 19:59:08 -0700

    Update shortflag.js

--- app/shortflag.js
+++ app/shortflag.js
@@ -1,8 +1,11 @@
 document.addEventListener("click", function() {
   try {
     let checkbox = document.querySelector("#short-style-checkbox");
-    window.xworker.short_style_flag = checkbox.checked;
-    window.simulationworker.short_style_flag = checkbox.checked; 
+    if (checkbox) {
+      window.short_style_flag = checkbox.checked;
+      window.xworker.short_style_flag = checkbox.checked;
+      window.simulationworker.short_style_flag = checkbox.checked; 
+    }
   } catch (e) {
     console.log(e);
   }
